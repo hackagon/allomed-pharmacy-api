27541d4be28d945bb892f64604412cc7ca7f0289
hackagon <phonghiavan@gmail.com>
2020-06-23 20:45:59 +0700

    basic product entity

--- src/entities/product.entity.ts
+++ src/entities/product.entity.ts
@@ -1,36 +1,69 @@
-import { Entity, PrimaryGeneratedColumn, Column, BaseEntity, ManyToOne, JoinColumn } from 'typeorm';
+import { Entity, Column, BaseEntity, ManyToOne, JoinColumn, PrimaryColumn } from 'typeorm';
 import { IngredientEntity } from './ingredient.entity';
+import { v4 as uuidv4 } from 'uuid';
 
 @Entity({
   name: "product"
 })
 export class ProductEntity extends BaseEntity {
-  @PrimaryGeneratedColumn()
-  id: number;
-
-  @Column({
-    name: "sku"
+  @PrimaryColumn({
+    default: uuidv4()
   })
+  id: string;
+
+  @Column()
   sku: string;
 
-  @Column({
-    name: "product_code"
-  })
-  productCode: string;
+  @Column()
+  product_code: string;
+
+  @Column()
+  product_name: string;
+
+  @Column()
+  brand_name: string;
+
+  @Column()
+  sub_brand_name: string;
+
+  @Column()
+  api_id: string;
+
+  @Column()
+  dosage_quantity: number;
+
+  @Column()
+  dosage_unit_id: string;
 
   @Column({
-    name: "brand_name"
+    name: "package_unit_id"
   })
-  brandName: string;
+  packageUnitId: string;
 
-  @Column({
-    name: "sub_brand_name"
-  })
-  subBrandName: string;
+  @Column()
+  dosage_form_id: string;
+
+  @Column()
+  package_id: string;
+
+  @Column()
+  manufacturing_enterprise_id: string;
+
+  @Column()
+  manufacturing_country_id: string;
+
+  @Column()
+  pharmaceutical_classification_id: string;
+
+  @Column()
+  store_id: string;
+
+  @Column()
+  national_product_code: string;
 
   @ManyToOne(type => IngredientEntity, ingredient => ingredient.products)
   @JoinColumn({
     name: "ingredient_id"
   })
-  ingredientId
+  ingredientId: string;
 }
