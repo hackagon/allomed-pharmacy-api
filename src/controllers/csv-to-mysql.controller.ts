6020c1e850482d164a929653078c0a341595b5c2
hackagon <phonghiavan@gmail.com>
2020-07-16 17:09:15 +0700

    clean code

--- src/controllers/csv-to-mysql.controller.ts
+++ src/controllers/csv-to-mysql.controller.ts
@@ -1,9 +1,5 @@
 import { Controller, Post } from "@nestjs/common";
-import * as csvtojson from "csvtojson";
-import * as path from "path";
 import * as _ from "lodash";
-import * as fs from "fs";
-import { v4 as uuidv4 } from 'uuid'
 import { CSVToMySQLService } from '../services/csv-to-mysql.service';
 
 @Controller("/csv-to-mysql")
