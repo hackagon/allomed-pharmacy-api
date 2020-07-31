af9a15e5ee17b505f2ba328edae7b78ca0f237de
hackagon <phonghiavan@gmail.com>
2020-07-31 16:09:58 +0700

    aadd procfile

---
+++ src/config/index.ts
@@ -0,0 +1,14 @@
+import * as path from "path";
+
+const envPath = path.join(__dirname, `../../.env.${process.env.NODE_ENV}`)
+
+require("dotenv").config({ path: envPath })
+
+export const config = {
+  type: process.env.DB_TYPE,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+}
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,12 +1,17 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { config } from "./index";
+import * as _ from "lodash";
+
+const { type, host, username, password, database, port } = config;
+console.log("type", type)
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: "mysql",
-  host: "localhost",
-  port: 3306,
-  username: "root",
-  password: "hackagon",
-  database: "allomed-pharmacy",
+  host,
+  port,
+  username,
+  password,
+  database,
   entities: [__dirname + "/../entities/*.entity.{js,ts}"],
   synchronize: true
 }
