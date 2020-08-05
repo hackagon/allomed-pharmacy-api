3a4b971e8196114b087e4eec496df83167578d97
Chau Nguyen <nguyentrungchau1996@gmail.com>
2020-08-05 16:31:56 +0700

    Update CSV file

--- src/controllers/csv-to-mysql.controller.ts
+++ src/controllers/csv-to-mysql.controller.ts
@@ -1,19 +1,21 @@
-import { Controller, Post } from "@nestjs/common";
-import * as _ from "lodash";
+import { Controller, Post } from '@nestjs/common';
+import * as _ from 'lodash';
 import { CSVToMySQLService } from '../services/csv-to-mysql.service';
 
-@Controller("/csv-to-mysql")
+@Controller('/csv-to-mysql')
 export class CSVToMySQLController {
-  constructor(
-    private csvService: CSVToMySQLService
-  ) { }
+  constructor(private csvService: CSVToMySQLService) {}
 
-  @Post("/")
+  @Post('/')
   async importCSVToMySQL(): Promise<any> {
     await Promise.all([
-      this.csvService.generalConvert("dosage_form"),
-      this.csvService.generalConvert("dosage_unit"),
-      this.csvService.generalConvert("product_classification")
-    ])
+      this.csvService.generalConvert('dosage_form'),
+      this.csvService.generalConvert('dosage_unit'),
+      this.csvService.generalConvert('product_classification'),
+      this.csvService.generalConvert('shape'),
+      this.csvService.generalConvert('color'),
+      this.csvService.generalConvert('flavor'),
+      this.csvService.generalConvert('volume'),
+    ]);
   }
 }
