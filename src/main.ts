5380a5d9e4435f0bf5ba0eae69ebe2a8c4ceec10
Van Pho <phonghiavan@gmail.com>
2020-08-23 09:54:02 +0700

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,8 @@ async function bootstrap() {
 
   app.setGlobalPrefix("/api");
 
+  app.enableCors();
+
   app.useGlobalPipes(
     new ValidationPipe({
       exceptionFactory: (validationErrors: ValidationError[] = []) => {
